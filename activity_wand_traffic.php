d036c089c2de100ac89caf82912768ff3ce0e2a9
Marcus Sternberg <marcus@taskito.de>
2012-05-13 13:54:06 +0200

    Hello

---
+++ activity_wand_traffic.php
@@ -0,0 +1,33 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Frameset//EN"
+   "http://www.w3.org/TR/html4/frameset.dtd">
+<html>
+<head>
+<meta http-equiv="refresh" content="300; URL=http://taskscout24.rz.is24.loc/activity_wand_traffic.php">
+<title>Frameset mit Sitemap</title>
+<style>
+#footer { font-size:200%;
+	  font-family: Arial,Sans-serif; 
+}
+img { width:660px;  }
+</style>
+</head>
+<body>
+<!--     <img src="http://hamcacti.rz.is24.loc/graph_image.php?local_graph_id=6770&rra_id=1&graph_height=200&graph_width=635&graph_nolegend=true" /> -->
+     <img src="http://bercacti.rz.is24.loc/graph_image.php?local_graph_id=3970&rra_id=1&graph_height=200&graph_width=635&graph_nolegend=true" />
+<div id="footer">
+        <!--#config timefmt="%d.%b " -->Aktualisiert:<!--#echo var="DATE_LOCAL" --><!--#config timefmt="%H:%M:%S %Z" --><!--#echo var="DATE_LOCAL" -->
+
+<?php
+###### Editnotes ####
+#$LastChangedDate: 2012-04-23 16:15:27 +0200 (Mo, 23 Apr 2012) $
+#$Author: msternberg $ 
+#####################
+# 
+$datum = date("d.m.Y");
+$uhrzeit = date("H:i");
+echo $datum," - ",$uhrzeit," Uhr";
+?>
+</div>
+</body>
+</html>
+
